8cfc99b64b9f5885b91c84ea02f8d61d9ed1712f
NikhilMali33 <nikhilmali5633@gmail.com>
2026-09-09 13:05:52 +0530

    Externalize API URL and CORS origins for deployment

--- sales-savvy-frontend/src/services/productService.js
+++ sales-savvy-frontend/src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8080/api/products";
+const API = import.meta.env.VITE_API_URL || "http://localhost:8080/api/products";
 
 export const getAllProducts = () =>
     axios.get(API);
